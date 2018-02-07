4bc6062837794c56a01960569398cb4f319a3c4e
velizargenov <velizar.genov@gmail.com>
2018-02-07 14:34:43 +0000

    Add a template and a new route * Add the first template file hellow.html * Add the /<name> roue * Add custom styles

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask('MyApp')
 
@@ -6,6 +6,15 @@ app = Flask('MyApp')
 @app.route('/')
 def hello():
     return 'Hello Flask! You\'re awesome!!!'
+
+
+@app.route('/<name>')
+def hello_someone(name):
+    return render_template('hello.html', name=name.title())
+
+
+def asd():
+    return 'hi'
 
 
 app.run(debug=True)

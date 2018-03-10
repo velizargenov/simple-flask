e595cfaa75c8bc3d4ba27dae27259e96d0837d63
velizargenov <velizar.genov@gmail.com>
2018-03-10 23:05:26 +0000

    Add json file with langauges data * Implement a route to handle selection of languages * Implement a route to handle displaying data of a language

--- hello.py
+++ hello.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask('MyApp')
 
 
 @app.route('/')
 def hello():
-    return 'Hello Flask! You\'re awesome!!!'
-
-
-@app.route('/<name>')
-def hello_someone(name):
-    return render_template('hello.html', name=name.title())
+    return render_template('index.html')
 
 
 @app.route('/contact')
@@ -22,6 +18,19 @@ def contact():
 def sign_up():
     form_data = request.form
     return render_template('thankyou.html', data=form_data)
+
+
+@app.route('/select-language')
+def select_language():
+    return render_template('select-language.html')
+
+
+@app.route('/language-details', methods=['POST'])
+def get_language_details():
+    data = json.load(open('static/js/data.json'))
+    language = request.form['language']
+    language_data = data[language]
+    return render_template('language-details.html', data=language_data)
 
 
 app.run(debug=True)
